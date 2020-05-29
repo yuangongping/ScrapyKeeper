f8afc2cd882b2ca13d6f350e4133853d8677e6d2
Taoz <371956576@qq.com>
2020-05-29 11:22:44 +0800

    fix data example

--- ScrapyKeeper/service/RedisSrv.py
+++ ScrapyKeeper/service/RedisSrv.py
@@ -5,8 +5,6 @@ import demjson
 
 
 class RedisSrv(object):
-    redis = redis
-
     @classmethod
     def getRedisUrls(self, args: dict):
         # 通过查询scheduler，用于获取配置参数
@@ -17,14 +15,14 @@ class RedisSrv(object):
         # 配置参数字符串对象 转 对象
         storage_management_form = demjson.decode(demjson.decode(scheduler.config).get("storage_management_form"))
         # 创建redis的链接
-        redis = self.redis.Redis(
+        redis_conn = redis.Redis(
             host=storage_management_form.get("redis").get("ip"),
             port=storage_management_form.get("redis").get("port"),
             db=0)
         # 获取待采队列的总数
-        total = redis.scard(args.get("project_name"))
+        total = redis_conn.scard(args.get("project_name"))
         # 从待采队里中随机取20个
-        data = redis.srandmember(args.get("project_name"), 20)
+        data = redis_conn.srandmember(args.get("project_name"), 20)
         res = ["当前待采集URL队列数量为: {}".format(total)]
         for i in data:
             res.append(str(i))
