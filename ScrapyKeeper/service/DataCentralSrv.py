3f1b9ff8c6ae9c333e47595408a4b5b98225d96f
yuangongping <1030617785@qq.com>
2020-06-01 08:49:08 +0800

    'fext：优化settings'

--- ScrapyKeeper/service/DataCentralSrv.py
+++ ScrapyKeeper/service/DataCentralSrv.py
@@ -1,8 +1,4 @@
-import psutil
-import datetime
-from ScrapyKeeper.service.ProjectSrv import ProjectSrv
-from ScrapyKeeper.service.ElkLogSrv import ElkLogSrv
-from ScrapyKeeper.model.Project import db, Project
+from ScrapyKeeper.model.Project import db
 from ScrapyKeeper.model.DataStorage import DataStorage
 from sqlalchemy import func
 from ScrapyKeeper.utils.date_tools import get_near_ndays
@@ -12,47 +8,31 @@ from ScrapyKeeper import app
 class DataCentralSrv:
     def get(self):
         data = {
-            "dataCount": self.get_all_data_count(),
+            "dataCount": self.get_data_count(),
             "data_size": self.get_data_size() + self.get_file_size(),
             "file_size": self.get_file_size()
         }
         return data
-
-    def get_cpu_state(self, interval=1):
-        return psutil.cpu_percent(interval)
-
-    def getMemorystate(self):
-        phymem = psutil.virtual_memory()
-        return {
-            "used": int(phymem.used / 1024 / 1024),
-            "total": int(phymem.total / 1024 / 1024)
-        }
-
-    def get_all_data_count(self):
-        name = app.config.get("DATASTORAGENAME")
-        sql = """SELECT TABLE_SCHEMA, TABLE_NAME, (TABLE_ROWS) FROM
-                    information_schema.TABLES
-                    WHERE TABLE_SCHEMA = '{}';""".format(name)
-        all = db.engine.execute(sql)
-        count = 0
-        for item in all:
-            count += item[2]
-        return count
+    
+    def get_data_info(self):
+        sql = """SELECT SUM(num), SUM(file_size) FROM datastorage;"""
+        data = db.session.excute()
+        return int(data) if data is not None else 0
 
     def get_data_size(self):
+        """
+        通过information_schema 表 获取指定库的占用空间大小
+        :return:
+        """
         name = app.config.get("DATASTORAGENAME")
         sql = """select round(sum(DATA_LENGTH / 1024 ), 2) as data
-        from information_schema.TABLES where
-        table_schema = '{}';""".format(name)
+          from information_schema.TABLES where
+          table_schema = '{}';""".format(name)
         all = db.engine.execute(sql)
         data = 0
         for item in all:
             data = int(item[0]) if item[0] else 0
         return data
-
-    def get_file_size(self):
-        data = db.session.query(func.sum(DataStorage.file_size)).scalar()
-        return int(data) if data is not None else 0
 
     def get_week_data(self):
         """
@@ -96,8 +76,3 @@ class DataCentralSrv:
             "xAxis": days,
             "yAxis": data_num
         }
-
-
-
-
-
